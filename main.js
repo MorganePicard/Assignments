9182c4ec23ff35a57dfd95e05eb269c8ce340ee7
Glitch (hello-jquery6) <none>
2020-10-18 14:18:16 +0000

    💆🍫 Checkpoint ./main.js:23476279/969

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 $.getJSON('https://api.coingecko.com/api/v3/exchange_rates', function(data) {
   console.log(data.rates);
 
-var object = {rates:btc, rates:name, rates:name};
-  
+var name = {name:"United Arab Emirates Dirham", name:"Argentine Peso", name:"Australian Dolla"};
+  for (var ){
+    console.log(`${rates[name]}`);
   
 });
 

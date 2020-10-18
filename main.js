db428a4c834d622e6ec1d71ae82e0f31c370a5d9
Glitch (hello-jquery6) <none>
2020-10-18 16:48:18 +0000

    ✈️💿 Checkpoint ./index.html:23476279/155 ./main.js:23476279/1863

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 $.getJSON("https://api.coingecko.com/api/v3/exchange_rates", function(data) {
   console.log(data.rates.btc.name);
+  
+  
+  
+  
+  
+  
+  
   $("#main").append(
     '<div class="row"><div class="three columns"><p> ' +
       data.rates.btc.name +

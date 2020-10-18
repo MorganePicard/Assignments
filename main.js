48f8f80dddff86c4bf2f4af8059fac66b6611b46
Glitch (hello-jquery6) <none>
2020-10-18 13:20:41 +0000

    💆🕴 Checkpoint ./main.js:23476279/248 ./index.html:23476279/206

--- main.js
+++ main.js
@@ -7,8 +7,11 @@ $.getJSON
   for(var i=0 < json.rates.length; i=i + 1) {
     var currencies = json.rates[i];
     
-$("#main").append("<p>" + rates.btc.value +"</p>");
+$("#main").append("<p>" + "btc" + rates.btc.name +"</p>");
+$("#main").append("<p>" + "eth" + rates.eth.name + "</p>");
+$("#main").append("<p>") + "ltc" + rates.ltc.name + "</p>");
     
+  }
 
 
 
